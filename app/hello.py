ae560f942a1dcfc807f564d26c5c11ef60a3d166
Engineer Intern <engineer-intern@gigacover.com>
2019-11-19 17:14:06 +0700

    add scenario endpoint

--- app/hello.py
+++ app/hello.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/scenario', methods=['POST'])
+def scenario():
+    url = request.form['url']
+    target = request.form['target_like']
+    return render_template('scenario.html')
